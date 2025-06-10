36e05e20c05ceeda399152a1b52c8b7f95fcbdcd
DwikiDharmawan22 <231712645@students.uajy.ac.id>
2025-06-10 23:32:46 +0700

    sama perbaiki api contact

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,11 +1,11 @@
-import { getContactData } from '@/app/lib/data2';
 import { NextResponse } from 'next/server';
+import { getContactData } from '@/app/lib/data2';
 
 export async function GET() {
   try {
     const data = await getContactData();
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error: 'Failed to fetch contact data' }, { status: 500 });
+    return NextResponse.json({ error: 'Gagal mengambil data kontak' }, { status: 500 });
   }
 }
