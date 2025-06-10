27b1cb2d143f88836b25f5179d9ad0e5b6f8653e
DwikiDharmawan22 <231712645@students.uajy.ac.id>
2025-06-10 23:32:30 +0700

    perbaiki page contact

--- app/dashboard/contact/page.tsx
+++ app/dashboard/contact/page.tsx
@@ -3,10 +3,9 @@ import { Contact } from '@/app/lib/definitions2';
 import { FaMapMarkerAlt, FaPhoneAlt, FaGlobe, FaFacebook, FaPinterest, FaWhatsapp, FaInstagram } from 'react-icons/fa';
 
 async function fetchContactData(): Promise<Contact> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-  const res = await fetch(`${baseUrl}/api/contact`, { cache: 'no-store' });
+  const res = await fetch('/api/contact', { cache: 'no-store' });
   if (!res.ok) {
-    throw new Error('Failed to fetch contact data');
+    throw new Error('Gagal mengambil data kontak');
   }
   return res.json();
 }
@@ -17,7 +16,7 @@ export default async function ContactPage() {
     <div className="min-h-full bg-gradient-to-b text-white overflow-x-hidden">
       <div className="px-6 py-8">
         <h1 className={`${irishGrover.className} text-4xl text-center font-bold mb-10`}>
-          VISIT OUR OFFLINE STORE
+          KUNJUNGI TOKO OFFLINE KAMI
         </h1>
 
         <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-10 items-center justify-center">
@@ -29,7 +28,7 @@ export default async function ContactPage() {
           </div>
 
           <div className="w-full md:w-1/2 rounded-xl p-6 border border-white shadow-lg">
-            <h2 className={`${irishGrover.className} text-2xl mb-4 text-center`}>CONTACT</h2>
+            <h2 className={`${irishGrover.className} text-2xl mb-4 text-center`}>KONTAK</h2>
             <div className={`${irishGrover.className} space-y-4 text-white/90 text-lg`}>
               <div className="flex items-center justify-center gap-4">
                 <FaMapMarkerAlt className="text-2xl mt-1" />
@@ -52,7 +51,7 @@ export default async function ContactPage() {
         <div className="px-6 py-3 mx-auto md:px-12">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="flex items-center gap-4 mb-2 md:mb-0">
-              <span className="font-semibold">Follow us</span>
+              <span className="font-semibold">Ikuti kami</span>
               <div className="flex gap-3 text-xl">
                 <FaFacebook />
                 <FaPinterest />
